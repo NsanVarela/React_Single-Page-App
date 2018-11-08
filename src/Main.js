0369071ff617bed047b252162828aabebac819de
NsanV <nsanvarela@gmail.com>
2018-11-08 14:51:24 +0100

    changing Stuff file in Info

---
+++ src/Main.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+import Home from "./Home";
+import Info from "./Info";
+import Contact from "./Contact";
+ 
+class Main extends Component {
+  render() {
+    return (
+      <HashRouter>
+        <div>
+          <h1>Simple SPA</h1>
+          <ul className="header">
+            <li><NavLink exact to="/">Home</NavLink></li>
+            <li><NavLink to="/info">Info</NavLink></li>
+            <li><NavLink to="/contact">Contact</NavLink></li>
+          </ul>
+          <div className="content">
+             <Route exact path="/" component={Home}/>
+             <Route path="/info" component={Info}/>
+             <Route path="/contact" component={Contact}/>
+          </div>
+        </div>
+      </HashRouter>
+    );
+  }
+}
+ 
+export default Main;
+
+
